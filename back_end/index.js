af89f6132d5ab89c4c0f44ad1a4eac3f8a045ba2
Achraf EL KADIRI <62653000+AchrafELKADIRI99@users.noreply.github.com>
2021-04-28 18:27:33 +0000

    added routes to index.js

--- back_end/index.js
+++ back_end/index.js
@@ -3,13 +3,19 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import hotelRoutes from './Routes/hotels.js';
+import voyagesRoutes from './Routes/voyages';
+import volsRoutes from './vols/voyages';
+
 
 
 const app = express();
 
 //starting path :  every route in hotel routes is gonna start with hotels  => localhost:5000/hotels
-
+//routes
 app.use('/hotels ' , hotelRoutes);
+app.use('/voyages ' , voyagesRoutes);
+app.use('/vols ' , volsRoutes);
+
 
 express.json({limit : "30mb" , extended : true});
 express.urlencoded({limit : "30mb" , extended : false});
