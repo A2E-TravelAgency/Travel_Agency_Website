dcff922d97395964a106f96763e72d08a10c8738
Achraf EL KADIRI <62653000+AchrafELKADIRI99@users.noreply.github.com>
2021-04-28 18:13:17 +0000

    added voyages.js post put delete

--- back_end/Routes/voyages.js
+++ back_end/Routes/voyages.js
@@ -6,6 +6,14 @@ router.get('/' , (req, res ) => {
 
     res.send('wokring ...');
 });
-
+app.post('/',  (req, res)=> {
+    res.send('Got a POST request');
+  });
+  app.put('/user',  (req, res) =>{
+    res.send('Got a PUT request at /user');
+  });
+  app.delete('/user',  (req, res)=> {
+    res.send('Got a DELETE request at /user');
+  });
 
 export default router;
