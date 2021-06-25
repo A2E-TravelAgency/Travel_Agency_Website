1d7b0b46696576fac2c2972745d01380a3d3fbcd
Achraf EL KADIRI <62653000+AchrafELKADIRI99@users.noreply.github.com>
2021-06-25 18:56:04 +0100

    added state to inputs for travels add

--- front_end1/src/App.js
+++ front_end1/src/App.js
@@ -1,10 +1,71 @@
+import React,{useState} from 'react';
+import Axios from 'axios';
 
 import './App.css';
 
 const  App =  () => {
+
+  const addToVoyages=()=>{
+
+  }
+
+
+  const [villeDep,setVilleDep] = useState('');
+  const [villeArr,setVilleArr] = useState('');
+  const [dateDep,setDateDep] = useState('');
+  const [dateArr,setDateArr] = useState('');
+  const [nbPlace,setNbPlace] = useState('');
+  const [price,setPrice] = useState('');
+
+
   return (
-    <div >
-      <h1>App</h1>
+    <div class="App" >
+      <h1>Management des voyages</h1>
+
+      
+        <label for="vd">Ville départ: </label>
+        <input
+          name="vd" type = "Text"
+          onChange={(event)=>{
+            setVilleDep(event.target.value);
+            }} 
+            />
+        <label for="va">Ville arrivé: </label>
+        <input name="va" type = "Text"
+            onChange={(event)=>{
+            setVilleArr(event.target.value);
+            }}
+            />
+        <label for="dd">Date départ: </label>
+        <input name="dd" type = "Date"
+         onChange={(event)=>{
+          setDateDep(event.target.value);
+          }}/>
+        <label for="da">Date départ: </label>
+        <input name="da" type = "Date"
+         onChange={(event)=>{
+          setDateArr(event.target.value);
+          }}
+        />
+        <label for="np">Nombre de place:  </label>
+        <input name="np" type = "Number"  min="0" max="100"
+         onChange={(event)=>{
+          setNbPlace(event.target.value);
+          }}
+        />
+        <label for="Prix">Prix: </label>
+        <input name="Prix" type = "Text" 
+        onChange={(event)=>{
+          setPrice(event.target.value);
+          }}
+        />
+        <button onClick={addToVoyages} > ADD</button>
+     
+      
+      
+
+
+
     </div>
   );
 }
